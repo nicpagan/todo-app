77c85fec55922a92b8c7f6e5f3ac5139600ae815
Nicole <nicxpagan@gmail.com>
2022-02-21 23:15:54 -0500

    heroku changes

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,14 @@
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+const { 
+  DATABASE_URL = "postgres://ceeptalg:O-gXT4JYCsy1xx792v0HFOijGRHOsg5j@jelani.db.elephantsql.com/ceeptalg" 
+} = process.env;
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: "postgres://ceeptalg:O-gXT4JYCsy1xx792v0HFOijGRHOsg5j@jelani.db.elephantsql.com/ceeptalg"
+    connection: DATABASE_URL
   },
   migrations: {
     directory: path.join(__dirname, "server", "api", "db", "migrations")
